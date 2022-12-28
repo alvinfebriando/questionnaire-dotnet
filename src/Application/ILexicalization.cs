9d3a66e7db3d200a3be2b3f303f4ffd1537ac8d8
Alvin Febriando <alvinfebriando@outlook.com>
2022-12-28 16:48:49 +0700

    Add ILexicalization interface

---
+++ src/Application/ILexicalization.cs
@@ -0,0 +1,11 @@
+﻿namespace Questionnaire.Application;
+
+public interface ILexicalization
+{
+    public string RndResponden();
+    public string RndMendapat();
+    public string RndTertinggi();
+    public string RndTerendah();
+    public string RndNilai();
+    public string GetStatus(double score);
+}
