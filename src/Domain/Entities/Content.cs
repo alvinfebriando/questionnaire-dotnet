8e6e9277693ec010a02965bcf1d60164ff3b7999
Alvin Febriando <alvinfebriando@outlook.com>
2022-12-28 17:41:19 +0700

    Add Content and Structure entities

---
+++ src/Domain/Entities/Content.cs
@@ -0,0 +1,22 @@
+﻿namespace Questionnaire.Domain.Entities;
+
+public class Content
+{
+    public string Place { get; set; }
+    public DateOnly Date { get; set; }
+    public string Subject { get; set; }
+    public string Respondent { get; set; }
+    public string Lecturer { get; set; }
+    public double AverageScore { get; set; }
+
+    public Content(string place, DateOnly date, string subject, string respondent, string lecturer,
+        double score)
+    {
+        Place = place;
+        Date = date;
+        Subject = subject;
+        Respondent = respondent;
+        Lecturer = lecturer;
+        AverageScore = score;
+    }
+}
---
+++ src/Domain/Entities/Structure.cs
@@ -0,0 +1,16 @@
+﻿namespace Questionnaire.Domain.Entities;
+
+public class Structure
+{
+    private readonly IList<string> _structures = new List<string>();
+
+    public void Add(string s)
+    {
+        _structures.Add(s);
+    }
+
+    public string Get(int index)
+    {
+        return _structures[index];
+    }
+}
