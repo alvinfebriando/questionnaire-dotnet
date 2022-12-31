497fd0ffcd2e24532d117db11154e04605878211
Alvin Febriando <alvinfebriando@outlook.com>
2022-12-31 11:56:32 +0700

    Add StructureRule class

---
+++ src/Application/Rule/IStructureRule.cs
@@ -0,0 +1,8 @@
+﻿using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Application.Rule;
+
+public interface IStructureRule
+{
+    public Structure Rule(Point point);
+}
---
+++ src/Application/Util.cs
@@ -0,0 +1,11 @@
+﻿namespace Questionnaire.Application;
+
+public static class Util
+{
+    public static T GetRandom<T>(IList<T> list)
+    {
+        Random rnd = new();
+        var index = rnd.Next(0, list.Count);
+        return list[index];
+    }
+}
