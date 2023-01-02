e3558814fce717e4219c8861981c6f514dd59446
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-02 18:37:46 +0700

    Add DocumentPlanning class

---
+++ src/Application/DocumentPlanning/DocumentPlanning.cs
@@ -0,0 +1,20 @@
+﻿using Questionnaire.Application.Rule;
+using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Application.DocumentPlanning;
+
+public class DocumentPlanning : IDocumentPlanning
+{
+    public Content DetermineContent(string place, DateOnly date, string subject, string respondent,
+        string lecturer, IEnumerable<Answer> answers, IContentRule rule)
+    {
+        var point = rule.Rule(answers);
+        var averageScore = answers.Select(x => x.Score).Average();
+        return new Content(place, date, subject, respondent, lecturer, averageScore, point);
+    }
+
+    public Structure DetermineStructure(Point point, IStructureRule rule)
+    {
+        return rule.Rule(point);
+    }
+}
---
+++ src/Application/DocumentPlanning/IDocumentPlanning.cs
@@ -0,0 +1,12 @@
+﻿using Questionnaire.Application.Rule;
+using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Application.DocumentPlanning;
+
+public interface IDocumentPlanning
+{
+    public Content DetermineContent(string place, DateOnly date, string subject, string respondent,
+        string lecturer, IEnumerable<Answer> answers, IContentRule rule);
+
+    public Structure DetermineStructure(Point point, IStructureRule rule);
+}
