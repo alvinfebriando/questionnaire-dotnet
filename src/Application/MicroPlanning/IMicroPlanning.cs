643616aff3035b2b66398eafbc7ce959131b69ee
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-03 18:34:46 +0700

    Add MicroPlanning class

---
+++ src/Application/MicroPlanning/IMicroPlanning.cs
@@ -0,0 +1,11 @@
+﻿using Questionnaire.Application.Topic;
+using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Application.MicroPlanning;
+
+public interface IMicroPlanning
+{
+    public Content Content { get; set; }
+    public Structure Structure { get; set; }
+    public IEnumerable<BaseTopic> Create();
+}
---
+++ src/Application/MicroPlanning/MicroPlanning.cs
@@ -0,0 +1,26 @@
+﻿using Questionnaire.Application.Topic;
+using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Application.MicroPlanning;
+
+public class MicroPlanning : IMicroPlanning
+{
+    public MicroPlanning(Content content, Structure structure, ILexicalization lex)
+    {
+        Content = content;
+        Structure = structure;
+        _lex = lex;
+    }
+
+    public Content Content { get; set; }
+    public Structure Structure { get; set; }
+    private readonly ILexicalization _lex;
+
+    public IEnumerable<BaseTopic> Create()
+    {
+        var overviewTopic = new OverviewTopic(Content, Structure, _lex);
+        var openingTopic = new OpeningTopic(Content, Structure, _lex);
+        var list = new List<BaseTopic> { overviewTopic, openingTopic };
+        return list;
+    }
+}
