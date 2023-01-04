aedbcefde5f9963ba0ebad00be066f5b53b44ade
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-04 16:40:23 +0700

    Add DetailPointMessage class and its own test

---
+++ Application.Tests/Message/DetailPointMessageTests.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using Questionnaire.Application.Dto;
+using Questionnaire.Application.Lexicalization;
+using Questionnaire.Application.Message;
+using Questionnaire.Domain.Entities;
+using Questionnaire.Domain.ValueObjects;
+using Xunit.Abstractions;
+
+namespace Questionnaire.Application.Tests.Message;
+
+public class DetailPointMessageTests
+{
+    private readonly ITestOutputHelper _output;
+    private readonly DetailPointMessage _sut;
+
+    public DetailPointMessageTests(ITestOutputHelper output)
+    {
+        _output = output;
+        var answer1 = new Answer(4.1,
+            new Question("title1", QuestionSection.KedisiplinanWaktu, "advice1"));
+        var answer2 = new Answer(4.1,
+            new Question("title2", QuestionSection.KedisiplinanWaktu, "advice2"));
+        var answers = new List<Answer> { answer1, answer2 };
+        var averagedAnswer = new AveragedAnswer(QuestionSection.KedisiplinanWaktu, 4.1, answers);
+        var dto = new PointDto("pak alvin", averagedAnswer);
+        var lex = new Mock<ILexicalization>();
+        lex.Setup(l => l.Search("tertinggi")).Returns("tertinggi");
+        lex.Setup(l => l.Search("terendah")).Returns("terendah");
+        _sut = new DetailPointMessage(dto, lex.Object);
+    }
+
+    [Fact]
+    public void Lexicalization_NoArgument_ReturnEmbeddedString()
+    {
+        // Arrange
+        var expected = _sut.Data.Answer.Answer.First().Question.Title;
+        // Act
+        var s = _sut.Lexicalization();
+        // Assert
+        Assert.Contains(expected, s);
+        _output.WriteLine(s);
+    }
+
+    [Fact]
+    public void EmbedComplement_MaxOption_ChangeComplementValue()
+    {
+        // Arrange
+        var option = new Option("max");
+        const string expected = "tertinggi";
+        // Act
+        _sut.EmbedComplement(option);
+        // Assert
+        Assert.Equal(expected, _sut.Complement[0]);
+        _output.WriteLine(_sut.Complement[0]);
+    }
+    [Fact]
+    public void EmbedComplement_MinOption_ChangeComplementValue()
+    {
+        // Arrange
+        var option = new Option("min");
+        const string expected = "terendah";
+        // Act
+        _sut.EmbedComplement(option);
+        // Assert
+        Assert.Equal(expected, _sut.Complement[0]);
+        _output.WriteLine(_sut.Complement[0]);
+    }
+}
---
+++ src/Application/Message/DetailPointMessage.cs
@@ -0,0 +1,37 @@
+﻿using Questionnaire.Application.Dto;
+using Questionnaire.Application.Lexicalization;
+using Questionnaire.Domain.ValueObjects;
+
+namespace Questionnaire.Application.Message;
+
+public class DetailPointMessage : BaseMessage<PointDto>, IComplemented
+{
+    private readonly ILexicalization _lex;
+
+    public DetailPointMessage(PointDto data, ILexicalization lex) : base(data)
+    {
+        _lex = lex;
+        Complement.Add("");
+    }
+
+    public override string Lexicalization()
+    {
+        var sentence =
+            $"berikut adalah rincian nilai yang {Complement[0]} ({Data.Answer.Section}):\n";
+        var s = Data.Answer.Answer.Select((a, index) =>
+            $"{index}. {a.Question.Title}: {a.Score}").ToList();
+        return sentence + string.Join("\n", s) + "\n";
+    }
+
+    public IList<string> Complement { get; set; } = new List<string>();
+
+    public void EmbedComplement(Option option)
+    {
+        Complement[0] = option.Description switch
+        {
+            "max" => _lex.Search("tertinggi"),
+            "min" => _lex.Search("terendah"),
+            _ => Complement[0]
+        };
+    }
+}
