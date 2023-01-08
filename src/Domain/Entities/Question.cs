8e95b6e3a62a810dfb835178bb71a2825ba4df9c
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-08 14:01:10 +0700

    Add Question id

--- src/Domain/Entities/Question.cs
+++ src/Domain/Entities/Question.cs
@@ -2,6 +2,14 @@ namespace Questionnaire.Domain.Entities;
 
 public class Question
 {
+    public Question(int id, string title, QuestionSection section, string advice)
+    {
+        Id = id;
+        Title = title;
+        Section = section;
+        Advice = advice;
+    }
+
     public Question(string title, QuestionSection section, string advice)
     {
         Title = title;
@@ -9,6 +17,7 @@ public class Question
         Advice = advice;
     }
 
+    public int Id { get; set; }
     public string Title { get; set; }
     public QuestionSection Section { get; set; }
     public string Advice { get; set; }
