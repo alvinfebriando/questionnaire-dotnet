9b9f2cc5adc9a1c9d950ec1d4198049dd65bef5e
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-08 15:03:40 +0700

    Round average score to 2 decimals

--- src/Application/DocumentPlanning/DocumentPlanning.cs
+++ src/Application/DocumentPlanning/DocumentPlanning.cs
@@ -8,8 +8,9 @@ public class DocumentPlanning : IDocumentPlanning
     public Content DetermineContent(string place, DateOnly date, string subject, string respondent,
         string lecturer, IEnumerable<Answer> answers, IContentRule rule)
     {
-        var point = rule.Rule(answers);
-        var averageScore = answers.Select(x => x.Score).Average();
+        var enumerable = answers.ToList();
+        var point = rule.Rule(enumerable);
+        var averageScore = Math.Round(enumerable.Select(x => x.Score).Average(),2);
         return new Content(place, date, subject, respondent, lecturer, averageScore, point);
     }
 
