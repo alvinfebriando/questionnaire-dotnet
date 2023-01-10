97bfc311135a3018335e1adcf62d2a350af9692a
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-10 11:05:26 +0700

    Add more synonym

--- src/Infrastructure/Persistence/InMemory/Lexicalization.cs
+++ src/Infrastructure/Persistence/InMemory/Lexicalization.cs
@@ -25,6 +25,8 @@ public class Lexicalization : ILexicalization
         _synonym.Add("tetapi", "namun");
         _synonym.Add("serta", "serta");
         _synonym.Add("dan juga", "serta");
+        _synonym.Add("dan juga", "kemudian");
+        _synonym.Add("dan juga", "lalu");
         _synonym.Add("untuk", "untuk");
         _synonym.Add("pada", "untuk");
         _synonym.Add("sedangkan", "sedangkan");
