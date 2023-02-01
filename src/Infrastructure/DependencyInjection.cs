4a792f410ddef96e8a400e17432c95f13f982ae0
Alvin Febriando <alvinfebriando@outlook.com>
2023-02-01 19:16:35 +0700

    Add Dependency Injection

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Questionnaire.Application.Lexicalization;
+using Questionnaire.Infrastructure.Persistence.InMemory;
+
+namespace Questionnaire.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<ILexicalization, Lexicalization>();
+        return services;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,9 @@
+using Questionnaire.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddInfrastructure();
 }
 
 var app = builder.Build();
