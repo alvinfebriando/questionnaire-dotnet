7870ec7c8f66eb1bc7428ed1809e2a27971695cf
Alvin Febriando <alvinfebriando@outlook.com>
2023-02-02 23:52:51 +0700

    Add error handling

---
+++ src/WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Questionnaire.WebApi.Controllers
+{
+    [Route("/error")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            return Problem(title: exception?.Message);
+        }
+    }
+}
