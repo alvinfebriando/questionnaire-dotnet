521a2d62de166af19c298c263a0dc99ed8502a39
Alvin Febriando <alvinfebriando@outlook.com>
2023-04-05 23:32:34 +0700

    Remove respondent field from database

---
+++ src/Infrastructure/Migrations/20230405160230_RemoveRespondent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Questionnaire.Infrastructure.Migrations
+{
+    public partial class RemoveRespondent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Respondent",
+                table: "Surveys");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Respondent",
+                table: "Surveys",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
