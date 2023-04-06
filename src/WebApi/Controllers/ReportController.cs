50d9485d0b122d103665b4d3920cf8c9371682a7
Alvin Febriando <alvinfebriando@outlook.com>
2023-04-06 10:52:46 +0700

    Remove obsolete report route

--- src/WebApi/Controllers/ReportController.cs
+++ src/WebApi/Controllers/ReportController.cs
@@ -1,7 +1,6 @@
 using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Questionnaire.Application.Report.Commands;
 using Questionnaire.Application.Report.Queries;
 using Questionnaire.WebApi.Dto;
 
@@ -18,14 +17,6 @@ public class ReportController : ControllerBase
     {
         _mediator = mediator;
         _mapper = mapper;
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Submit([FromBody] ReportRequest request)
-    {
-        var command = _mapper.Map<ReportCommand>(request);
-        var result = await _mediator.Send(command);
-        return Ok(_mapper.Map<ReportResponse>(result));
     }
 
     [HttpGet("{surveyId:guid}")]
