15c68a533647d86d8471cd4f5f798d46e7a60541
Alvin Febriando <alvinfebriando@outlook.com>
2023-05-29 21:29:29 +0700

    Add survey answered by data

---
+++ src/Infrastructure/Persistence/Postgres/Configuration/UserEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Questionnaire.Domain.Entities;
+
+namespace Questionnaire.Infrastructure.Persistence.Postgres.Configuration;
+
+public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder
+            .HasMany(u => u.Surveys)
+            .WithMany(s=>s.AnsweredBy);
+    }
+}
