bd327a564b1ccf6c2ec87b9bb093c3151c7a0762
Alvin Febriando <alvinfebriando@outlook.com>
2023-06-05 03:53:13 +0700

    Get list of answered survey by user

---
+++ src/Application/SurveyManagement/Queries/GetSurveyAnsweredByUserQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Questionnaire.Application.SurveyManagement.Queries;
+
+public record GetSurveyAnsweredByUserQuery(Guid UserId) : IRequest<IEnumerable<Guid>>;
---
+++ src/Application/SurveyManagement/Queries/GetSurveyAnsweredByUserQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Questionnaire.Application.Data;
+
+namespace Questionnaire.Application.SurveyManagement.Queries;
+
+public class
+    GetSurveyAnsweredByUserQueryHandler : IRequestHandler<GetSurveyAnsweredByUserQuery,
+        IEnumerable<Guid>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetSurveyAnsweredByUserQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Guid>> Handle(
+        GetSurveyAnsweredByUserQuery request,
+        CancellationToken cancellationToken)
+    {
+        var answers = await _context.Answers
+            .Include(a => a.AnsweredByUser)
+            .Include(a => a.SurveyQuestion)
+            .Where(a=>a.AnsweredByUser.Id == request.UserId)
+            .ToListAsync(cancellationToken);
+
+        return answers.Select(s => s.SurveyQuestion.SurveyId);
+    }
+}
